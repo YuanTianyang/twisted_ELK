6905cbaca9f071153653655506e5574cbdaae5d9
Yuan TianYang <woaiyatou449534640@gmail.com>
2017-12-22 11:54:44 +0800

    Low-level client version

--- src/main/java/me/ELK/test/lowLevelClient/ELKRestClient.java
+++ src/main/java/me/ELK/test/lowLevelClient/ELKRestClient.java
@@ -1,9 +1,16 @@
-package me.ELK.test.client;
+package me.ELK.test.lowLevelClient;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyStore;
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+
+import javax.net.ssl.SSLContext;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
@@ -20,6 +27,8 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.HttpAsyncResponseConsumerFactory;
 import org.elasticsearch.client.HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory;
@@ -27,9 +36,8 @@ import org.elasticsearch.client.Response;
 import org.elasticsearch.client.ResponseListener;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
-
-import com.sun.xml.internal.stream.Entity;
-
+import org.elasticsearch.client.sniff.SniffOnFailureListener;
+import org.elasticsearch.client.sniff.Sniffer;
 
 public class ELKRestClient {
 
@@ -89,18 +97,57 @@ public class ELKRestClient {
 					
 				});
 				
+				/*
+				 * 通过提供一个HttpClientConfigCallback，并通过它的构建器构建RestClient，去配置基本的身份验证。
+				 * 这个接口有一个方法去接收一个org.apache.http.impl.nio.client.HttpAsyncClientBuilder实例来作为参数，并且返回值类型也是这个实例。
+				 * 可以修改http客户端构建器然后返回。在下面的示例中我们设置了一个需要基本验证的缺省凭证提供者。
+				 */
 				final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
 				credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("Twisted", "4495346"));
 				restClientBuilder.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
 					
-					@Override
 					public HttpAsyncClientBuilder customizeHttpClient(
 							HttpAsyncClientBuilder httpClientBuilder) {
-						// TODO Auto-generated method stub
-						return null;
+						httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+						return httpClientBuilder;
 					}
-				})
+				});
 				
+				/*
+				 * 抢占式身份验证可以被禁用，这意味着每个请求将在没有认证头的情况下被发送，以查看它是否被接受，
+				 * 当接收到一个HTTP 401响应时，它会以基本身份验证头重新发送相同的请求。
+				 * 如果你想这样做，你可以HttpAsyncClientBuilder去禁用它。
+				 */
+				credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("Twisted", "4495346"));
+				restClientBuilder.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+					
+					public HttpAsyncClientBuilder customizeHttpClient(
+							HttpAsyncClientBuilder httpClientBuilder) {
+						//禁用抢占式身份验证。
+						httpClientBuilder.disableAuthCaching();
+						httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+						return httpClientBuilder;
+					}
+				});
+				
+				/*
+				 * HttpClientConfigCallback也能够配置通信加密。
+				 * org.apache.http.impl.nio.client.HttpAsyncClientBuilder接收许多种通信加密方法：setSSLContext, setSSLSessionStrategy 和 setConnectionManager，最不重要的是最前面的。
+				 *  
+				 */
+				KeyStore trustStore = KeyStore.getInstance("jks");
+				Path keyStorePath = Paths.get("密钥路径");
+				try (InputStream is = Files.newInputStream(keyStorePath)){
+//					trustStore.load(is,keyStorePass.toCharArray());
+				}
+				SSLContextBuilder sslBuilder = SSLContexts.custom().loadTrustMaterial(trustStore,null);
+				final SSLContext sslContext = sslBuilder.build();
+				restClientBuilder.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+					public HttpAsyncClientBuilder customizeHttpClient(
+							HttpAsyncClientBuilder httpClientBuilder) {
+						return httpClientBuilder.setSSLContext(sslContext);
+					}
+				});
 				
 				
 				restClient = restClientBuilder.build();
@@ -271,6 +318,43 @@ public class ELKRestClient {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+	
+	public void setSniffer(RestClient restClient){
+		
+		/*
+		 * 一旦一个RestClient被创建后并初始化后，一个Sniffer就可以去关联他。
+		 * 这个Sniffer会使用提供的RestClient周期性(默认为五分钟)从集群中获取当前节点的列表，并通过调用RestClient#setHosts来更新它们。
+		 */
+		Sniffer sniffer = Sniffer.builder(restClient).build();
+		
+		//Sniffer更新节点默认是5分钟。这个间隔可以通过（设置毫秒）提供的时间来自定义。
+		Sniffer sniffer2 = Sniffer.builder(restClient)
+				.setSniffIntervalMillis(60000).build();
+		
+		SniffOnFailureListener sniffOnFailureListener = new SniffOnFailureListener();
+		RestClient restClient2 = RestClient.builder(new HttpHost("localhost", 9200))
+				.setFailureListener(sniffOnFailureListener)
+				.build();
+		Sniffer sniffer3 = Sniffer.builder(restClient2)
+				.setSniffAfterFailureDelayMillis(30000)
+				.build();
+		sniffOnFailureListener.setSniffer(sniffer3);
+		
+		
+		
+		/*
+		 * 重要的是关闭Sniffer以便后台线程得到适当的关闭，并且它的资源也会全部释放。
+		 * Sniffer对象应该与RestClient有相同的生命周期,并且在客户端之前关闭。
+		 */
+		try {
+			sniffer.close();
+			restClient.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 	
